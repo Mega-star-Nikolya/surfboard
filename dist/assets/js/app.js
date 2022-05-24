e747c3b381227fdc79d7f8f4be11d3e312331b45
Nikolay Ivchenko <mega-star@bk.ru>
2022-05-24 21:11:03 +0300

    Добавление аккардиона

--- dist/assets/js/app.js
+++ dist/assets/js/app.js
@@ -47,7 +47,7 @@ function showSlides(n) {
 
 // Бургер меню с открытием навигации начало
 $("#jsburger").on("click", function(event) {
-event.preventDefault();
+    event.preventDefault(); // запрещаем перекидывать вверх или перезагрузать страницу
 
 $(this).toggleClass("active");
 $("#jsnav").toggleClass("active");
@@ -75,6 +75,7 @@ function currentItem(s) {
 function showShopItem(s) {
     var element;
     var shopItem = document.getElementsByClassName("shop__item");
+    var shopBtn = document.getElementsByClassName("slider__carousel-btn");
     if (s > shopItem.length) {
         slideShopIndex = 1
     }
@@ -89,6 +90,42 @@ function showShopItem(s) {
 }
 // Секция shop со слайдером - сам слайдер -конец-
 
+
+// Секция Team аккардион -начало-
+const team = document.querySelector('.team');
+/* Функция для открытия контента по нажатию на ссылку */
+function openItem(button) {
+    const contentWrap = button.nextElementSibling;
+    const content = contentWrap.firstElementChild;
+    const currentHeight = content.offsetHeight;
+
+    contentWrap.style.height = currentHeight + 'px';
+    button.classList.add('team__name--active', 'has-name--active');
+}
+
+/* Функция для закрытия контента по нажатию на ссылку */
+function closeItem(button) {
+    if (!button) return;
+    const contentWrap = button.nextElementSibling;
+    contentWrap.style.height = 0;
+    button.classList.remove('team__name--active', 'has-name--active');
+}
+
+team.addEventListener('click', function (event) {
+    event.preventDefault(); // запрещаем перекидывать вверх или перезагрузать страницу
+    const target = event.target;
+    const activeItem = document.querySelector('.team__name--active', '.has-name--active');
+
+    if (target.classList.contains('team__name')) {
+        if (target.classList.contains('team__name--active','has-name--active')) {
+            closeItem(target);
+        } else {
+            closeItem(activeItem);
+            openItem(target);
+        }
+    }
+})
+// Секция Team аккардион -конец-
 
 
 //# sourceMappingURL=app.js.map
