63d563dd48702653d4ff89332d31cb815186eb2c
Nikolay Ivchenko <mega-star@bk.ru>
2022-05-26 06:37:46 +0300

    Добавление в form окна об отправке

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -122,3 +122,67 @@ team.addEventListener('click', function (event) {
     }
 })
 // Секция Team аккардион -конец-
+
+
+// Секция form модельное окно -начало-
+const validateFields = (form, fieldsArray) => {
+
+    fieldsArray.forEach(field => {
+        field.removeClass("input-error");
+        if (field.val().trim() === "") {
+            field.addClass("input-error");
+        }
+    });
+    const errorFields = form.find(".input-error");
+    return errorFields.length === 0;
+};
+
+$('.form').submit(e => {
+    e.preventDefault(); // запрещаем перекидывать вверх или перезагрузать страницу
+    const form = $(e.currentTarget);
+    const name = form.find("[name='name']");
+    const phone = form.find("[name='tel']");
+    const comment = form.find("[name='comment']");
+    const to = form.find("[name='to']");
+
+    const modal = $("#modal");
+    const content = modal.find(".modal__content");
+
+    modal.removeClass("error-modal");
+
+    const isValid = validateFields(form, [name, phone, comment, to]);
+
+    if (isValid) {
+        const request = $.ajax({
+            url: "https://webdev-api.loftschool.com/sendmail",
+            method: "post",
+            data: {
+                name: name.val(),
+                phone: phone.val(),
+                comment: comment.val(),
+                to: to.val(),
+            },
+        });
+        request.done(data => {
+            content.text(data.message);
+        });
+        request.fail(data => {
+            const message = data.responseJSON.message;
+            content.text(message);
+            modal.addClass("error-modal");
+        });
+        request.always(() => {
+            $.fancybox.open({
+                src: "#modal",
+                type: "inline"
+            });
+        });
+    }
+});
+
+$(".js-submit-btn").click(e => {
+    e.preventDefault();
+
+    $.fancybox.close();
+})
+// Секция form модельное окно -конец-
