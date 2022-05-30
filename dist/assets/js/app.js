a76b93ee3fd22301e931d9e607325a217cc06779
Nikolay Ivchenko <mega-star@bk.ru>
2022-05-30 20:21:33 +0300

    Загрузка пятой ветки - видео проигрыватель

--- dist/assets/js/app.js
+++ dist/assets/js/app.js
@@ -2,7 +2,6 @@ var __webpack_exports__ = {};
 /*!******************************!*\
   !*** ./src/assets/js/app.js ***!
   \******************************/
-
 // Слайдер в секции с отзывами начало
 /* Индекс слайда по умолчанию */
 var slideIndex = 1;
@@ -191,7 +190,7 @@ $(".js-submit-btn").click(e => {
 });
 // Секция form модельное окно -конец-
 
-// Секция секция меню с вертикальным аккордеонам -начало-
+// Секция меню с вертикальным аккордеонам -начало-
 
 const menu = document.getElementById('horixontalMenu');
 const items = document.querySelectorAll(".menu__item");
@@ -260,5 +259,70 @@ window.addEventListener('resize', () => {
 }
 });
 // Секция меню с вертикальным аккордеонам -конец-
+
+// Секция видео проигрыватель -начало-
+
+/* Большая кнопка воспроизведения */
+const playBtn = document.querySelector(".video__player-img");
+
+/* Малая кнопка воспроизведения */
+const playerPlayBtn = document.querySelector(".duration__img");
+
+/* Само меню */
+const video = document.getElementById('player');
+
+/* input с помощью которого выставляется время воспроизведения */
+const durationControl = document.getElementById('durationLevel');
+
+/* input с помощью которого выставляется громкость */
+const soundControl = document.getElementById('micLevel');
+
+/* Обертка управления громкостью */
+const soundBtn = document.getElementById('soundBtn');
+
+/* Кнопка с динамиком с помощью которой мы будем включать или выключать громкость */
+const dynamicBtn = document.getElementById('dynamic');
+
+/* Индификатор интервала, который будет отрисовывать текущее время воспроизведения */
+let intervalId;
+
+/* Переменная для сохранения уровня громкости перед выключением звука */
+let soundLevel;
+
+
+window.addEventListener('load', () => {
+
+    /* При клике на само видео выполняем функцию playStop */
+    video.addEventListener("click", playStop);
+
+    /* Находи все кнопки управления play/pause */
+    let playButtons = document.querySelectorAll(".play");
+    for (let i = 0; i < playButtons.length; i++) {
+        /* При клике на эти кнопки выполняем функию playStop */
+        playButtons[i].addEventListener("click", playStop);
+    }
+});
+
+/* Функция воспроизводить/поставить на паузу */
+function playStop() {
+    /* Устанавливает активные классы на элементы чтоб показать возспроизведение */
+    playBtn.classList.toggle("video__player-img--active");
+    playerPlayBtn.classList.toggle("active");
+
+    /* Если видео на паузе */
+    if (video.paused) { 
+        video.play(); // Запускаем его
+        // Запускаем отрисовку ползунка воспроизведения
+        intervalId = setInterval(updateDuration, 1000 / 60);
+        // Если видео не на паузе
+    } else {
+        // Останавливаем отрисовку ползунка времени воспроизведения
+        clearInterval(intervalId);
+        // Ставим видео на паузу
+        video.pause();
+    }
+
+}
+// Секция видео проигрыватель -конец-
 
 //# sourceMappingURL=app.js.map
