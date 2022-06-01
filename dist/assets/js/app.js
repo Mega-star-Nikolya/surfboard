02d4ff15f7aee2a01a321f99e909e7a1896963be
Nikolay Ivchenko <mega-star@bk.ru>
2022-06-01 20:56:05 +0300

    Заливка секции с картой

--- dist/assets/js/app.js
+++ dist/assets/js/app.js
@@ -160,6 +160,51 @@ window.addEventListener('resize', () => {
 }();
 // This entry need to be wrapped in an IIFE because it need to be isolated against other entry modules.
 !function() {
+/*!******************************!*\
+  !*** ./src/assets/js/map.js ***!
+  \******************************/
+let myMap;
+
+const init = () => {
+  myMap = new ymaps.Map("map", {
+
+    // Задаем кардинаты
+    center: [55.752004, 37.576133],
+
+    // уменьшаем или увеличиваем масштаб
+    zoom: 15,
+  
+    // отключаем все ненужные элементы - Пробки
+    controls: []
+  });
+
+  // Задаем точки геолокации можно не одну
+  const coords = [
+    [55.752004, 37.576133]
+  ];
+
+  const myCollection = new ymaps.GeoObjectCollection({}, {
+    draggable: false,
+    iconLayout: 'default#image',
+    iconImageHref: './assets/images/map/map_click.png',
+    iconImageSize: [58, 73],
+    iconImageOffset: [-35, -52]
+  });
+
+  coords.forEach(coord => {
+    myCollection.add(new ymaps.Placemark(coord));
+  });
+
+  myMap.geoObjects.add(myCollection);
+
+  // Отключаем возможность прокрутки 
+  // myMap.behaviors.disable('scrollZoom');
+}
+
+ymaps.ready(init);
+}();
+// This entry need to be wrapped in an IIFE because it need to be isolated against other entry modules.
+!function() {
 /*!****************************************!*\
   !*** ./src/assets/js/sliderReviews.js ***!
   \****************************************/
